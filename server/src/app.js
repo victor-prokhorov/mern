c893fad5f748b230d7a0d48bcaad4dd73ac448dd
Victor Prokhorov <mail@victorprokhorov.com>
2026-08-15 09:33:33 +0200

    feat: orders (#5)
    * test: failing order endpoint tests
    
    cd server && npm test
    
      21 passing (1s)
      8 failing
    
      1) orders
           creates an order priced from the database:
    
          AssertionError: expected Response{ _events: {}, ...(34), ...(2) } to have status code 201 but got 404
    
      2) orders
           ignores prices sent by the client:
         AssertionError: expected undefined to equal 44
    
      3) orders
           empties the cart:
    
          AssertionError: expected [ { ...(2) }, { ...(2) } ] to have a length of +0 but got 2
    
      4) orders
           rejects an empty cart:
    
          AssertionError: expected Response{ ...(34), ...(2) } to have status code 400 but got 404
    
      5) orders
           rejects an unknown user:
         AssertionError: expected undefined to equal 'user not found'
    
      6) orders
           rejects a missing customer address:
    
          AssertionError: expected Response{ ...(34), ...(2) } to have status code 400 but got 404
    
      7) orders
           returns a stored order:
    
          AssertionError: expected Response{ ...(34), ...(2) } to have status code 200 but got 404
    
      8) orders
           returns 404 for an unknown order:
         AssertionError: expected undefined to equal 'order not found'
    
    All routes 404 because src/models/order.js and src/routes/orders.js do not exist yet.
    
    * feat: order placement and lookup

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import products from './routes/products.js'
 import auth from './routes/auth.js'
 import cart from './routes/cart.js'
+import orders from './routes/orders.js'
 import { errorHandler } from './middleware/error.js'
 
 const app = express()
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use('/api/products', products)
 app.use('/api/auth', auth)
 app.use('/api/cart', cart)
+app.use('/api/orders', orders)
 app.use(errorHandler)
 
 export default app
